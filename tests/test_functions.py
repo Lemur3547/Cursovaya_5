e8249fe2a63d8911cf8b69f4f9a2dee3729edd22
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-05-11 17:35:41 +0500

    Написаны тесты для файла functions.py Добавлены новые зависимости, необходимые для тестирования

---
+++ tests/test_functions.py
@@ -0,0 +1,57 @@
+import pytest
+
+from src.functions import presentation
+
+
+@pytest.fixture()
+def rows():
+    return [('Junior Software Engineer', 'Magis Corp', 500, 800, 'USD', 'https://hh.ru/vacancy/98427645'),
+            ('Back End разработчик', 'ROBOUP', None, 4500000, 'UZS', 'https://hh.ru/vacancy/98654688'),
+            ('Стажер-программист', 'Контора геймдева', 2500, None, 'BYR', 'https://hh.ru/vacancy/98346328'),
+            ('Разработчик Python Junior', 'Казтелепорт', None, None, None, 'https://hh.ru/vacancy/98383317')]
+
+
+def test_presentation(rows):
+    assert presentation(rows) == print('''Junior Software Engineer
+                                    Magis Corp
+                                    500 - 800 USD
+                                    Ссылка на вакансию https://hh.ru/vacancy/98427645
+                                    
+                                    Back End разработчик
+                                    ROBOUP
+                                    3000000.0 - 4500000.0 UZS
+                                    Ссылка на вакансию https://hh.ru/vacancy/98654688
+                                    
+                                    Стажер-программист
+                                    Контора геймдева
+                                    От 2500 BYR
+                                    Ссылка на вакансию https://hh.ru/vacancy/98346328
+                                    
+                                    Разработчик Python Junior
+                                    Казтелепорт, АО
+                                    Не указана
+                                    Ссылка на вакансию https://hh.ru/vacancy/98383317
+                                    ''')
+
+
+'''
+Junior Software Engineer (Начинающий специалист)
+Magis Corp
+500 - 800 USD
+Ссылка на вакансию https://hh.ru/vacancy/98427645
+
+Back End разработчик
+ROBOUP
+3000000.0 - 4500000.0 UZS
+Ссылка на вакансию https://hh.ru/vacancy/98654688
+
+Разработчик Python Junior (Backend)
+Казтелепорт, АО
+Не указана
+Ссылка на вакансию https://hh.ru/vacancy/98383317
+
+Стажер-программист
+Технологии пользовательских интерфейсов
+От 2500 BYR
+Ссылка на вакансию https://hh.ru/vacancy/98346328
+'''
